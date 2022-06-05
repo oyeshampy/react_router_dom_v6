1ad8f5158823089844cb372061fb59f263d4eefb
Puneet Sharma <puneet.sharma@check24.de>
2022-06-05 14:01:52 +0200

    added Routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Link, Route, Routes} from 'react-router-dom';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import About from './pages/About';
+import ErrorPage from './pages/ErrorPage';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <ul style={{float: 'left', listStyleType:'none'}}>
+          <li><Link to={'/'}>Home</Link></li>
+          <li><Link to={'/profile'}>Profile</Link></li>
+          <li><Link to={'/about'}>About</Link></li>
+        </ul>
+        <Routes>
+          <Route path={'/'} element={<Home/>}/>
+          <Route path={'/profile'} element={<Profile/>}/>
+          <Route path={'/about'} element={<About/>}/>
+          <Route path={'/*'} element={<ErrorPage/>}/>
+        </Routes>
+      </Router>
+
+      <footer>
+        this is footer
+      </footer>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/pages/Profile.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <div>
+      this is profile page<br/>
+      <button>Go to About</button>
+    </div>
+  );
+};
+
+export default Profile;
