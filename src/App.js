780c3522532edafc36aba4fd458e98e423f4821f
Puneet Sharma <puneet.sharma@check24.de>
2022-06-10 11:54:36 +0200

    added Routes useNavigate

--- src/App.js
+++ src/App.js
@@ -7,25 +7,27 @@ import ErrorPage from './pages/ErrorPage';
 
 const App = () => {
   return (
-    <div className="App">
-      <Router>
-        <ul style={{float: 'left', listStyleType:'none'}}>
+    <Router>
+      <div className="App">
+        <ul style={{
+          float: 'left',
+          listStyleType: 'none',
+        }}>
           <li><Link to={'/'}>Home</Link></li>
           <li><Link to={'/profile'}>Profile</Link></li>
           <li><Link to={'/about'}>About</Link></li>
         </ul>
-        <Routes>
+        <Routes style={{float:'right'}}>
           <Route path={'/'} element={<Home/>}/>
           <Route path={'/profile'} element={<Profile/>}/>
           <Route path={'/about'} element={<About/>}/>
           <Route path={'/*'} element={<ErrorPage/>}/>
         </Routes>
-      </Router>
-
-      <footer>
-        this is footer
-      </footer>
-    </div>
+        <footer style={{clear:'both'}}>
+          this is footer
+        </footer>
+      </div>
+    </Router>
   );
 };
 
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import {useNavigate} from 'react-router-dom';
 
 const Profile = () => {
   return (
     <div>
       this is profile page<br/>
-      <button>Go to About</button>
+      <button onClick={() => useNavigate('/about')}>Go to About</button>
     </div>
   );
 };
